ee62cdd01d447c026c1132bf4f8574e25cf258b2
EmilioPFematt <supermageblaziken@gmail.com>
2024-11-16 02:05:23 -0600

    fixes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const http = require('http');
 const { createServer } = require('node:http');
 const { join } = require('node:path');
 const { Server } = require('socket.io');
 
 const app = express();
-const server = createServer(app);
+const server = http.createServer(app);
 const io = new Server(server, {
   connectionStateRecovery: {}
 });
@@ -23,6 +24,6 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+server.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
 });
